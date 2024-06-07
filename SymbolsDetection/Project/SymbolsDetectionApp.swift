19ad5d3959b062e7f7737025b9c5209e1f0212c5
Dmitriy Soloshenko <demonsoftinc@gmail.com>
2024-06-07 10:55:41 +0300

    initial commit

---
+++ SymbolsDetection/Project/SymbolsDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolsDetectionApp.swift
+//  SymbolsDetection
+//
+//  Created by Dmitriy Soloshenko on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SymbolsDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
